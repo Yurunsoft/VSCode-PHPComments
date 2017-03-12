20ea755b30e7d0449690857cd15bfdf76559651f
宇润 <admin@yurunsoft.com>
2017-03-12 13:33:32 +0800

    新增通用注释功能

--- out/src/extension.js
+++ out/src/extension.js
@@ -1,66 +1,58 @@
 'use strict';
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-
+const vscode = require("vscode");
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-
+function activate(context) {
     const commandRegistration = vscode.commands.registerCommand('extension.addPHPComments', () => {
         // The code you place here will be executed every time your command is executed
         // Display a message box to the user
         // vscode.window.showInformationMessage(vscode.window.activeTextEditor.document.fileName);
         // vscode.window.activeTextEditor.edit(()=>{})
-        vscode.window.activeTextEditor.edit((editBuilder: vscode.TextEditorEdit) => {
-            var positionStart = new vscode.Position(vscode.window.activeTextEditor.selection.start.line,0);
-            var positionEnd = new vscode.Position(vscode.window.activeTextEditor.selection.start.line+1,0);
-            var content = vscode.window.activeTextEditor.document.getText(new vscode.Range(positionStart,positionEnd));
+        vscode.window.activeTextEditor.edit((editBuilder) => {
+            var positionStart = new vscode.Position(vscode.window.activeTextEditor.selection.start.line, 0);
+            var positionEnd = new vscode.Position(vscode.window.activeTextEditor.selection.start.line + 1, 0);
+            var content = vscode.window.activeTextEditor.document.getText(new vscode.Range(positionStart, positionEnd));
             var matches = content.match(/function ([^\s(]+)\(/);
             var tabMatches = content.match(/^\s+/);
             var tab = '';
-            if(null !== tabMatches)
-            {
+            if (null !== tabMatches) {
                 tab = tabMatches[0];
             }
-            if(null === matches)
-            {
+            if (null === matches) {
                 // 万能注释
                 var insertContent = tab + '/**\r\n' + tab + ' * \r\n' + tab + ' * @var mixed\r\n' + tab + ' */';
-                editBuilder.insert(positionStart,insertContent + '\r\n');
+                editBuilder.insert(positionStart, insertContent + '\r\n');
             }
-            else
-            {
+            else {
                 var methodName = matches[1];
                 matches = content.match(/\(([^)]*)\)/);
-                if(null === matches || matches.length <= 1)
-                {
+                if (null === matches || matches.length <= 1) {
                     return;
                 }
                 var insertContent = tab + '/**\r\n';
                 tab += ' ';
-                insertContent += tab + '* '+methodName+'\r\n';
+                insertContent += tab + '* ' + methodName + '\r\n';
                 matches = matches[1].match(/(\$[^\s,\)]+)/g);
-                if(null !== matches)
-                {
-                    for(var i=0;i<matches.length;i++)
-                    {
+                if (null !== matches) {
+                    for (var i = 0; i < matches.length; i++) {
                         insertContent += tab + '* @param mixed ' + matches[i] + ' \r\n';
                     }
                 }
                 insertContent += tab + '* @return mixed \r\n';
                 insertContent += tab + '*/';
-                editBuilder.insert(positionStart,insertContent + '\r\n');
+                editBuilder.insert(positionStart, insertContent + '\r\n');
             }
         }, {
             undoStopBefore: true,
             undoStopAfter: true
         });
     });
-
-
 }
-
+exports.activate = activate;
 // this method is called when your extension is deactivated
-export function deactivate() {
+function deactivate() {
 }
+exports.deactivate = deactivate;
+//# sourceMappingURL=extension.js.map
